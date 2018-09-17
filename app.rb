910b3784e61f55bc34ddba812ea046c9c7961570
Damion Hardy <damionhardy@Damos-MacBook-Pro-8.local>
2018-09-17 17:34:22 +0100

    committing Sinatra: Introducing params

--- app.rb
+++ app.rb
@@ -6,7 +6,13 @@ get '/' do
   "Hello!"
 end
 
-get '/cat' do
+get '/random-cat' do
   @cat_name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get'/named-cat' do
+  p params
+  @cat_name = params[:name]
   erb(:index)
 end
