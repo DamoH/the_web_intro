4c676875b6bd7cb1827bee305b87b37fa9eeb8ae
Damion Hardy <damionhardy@Damos-MacBook-Pro-8.local>
2018-09-17 16:43:42 +0100

    Sinatra - views... small changes

--- app.rb
+++ app.rb
@@ -6,16 +6,6 @@ get '/' do
   "Hello!"
 end
 
-get '/secret' do
-  "WHAT ????"
-end
-
-get '/new' do
-  "this is NEEEWWWW!!!"
-end
-
 get '/cat' do
-  "<div>
-    <img src='http://bit.ly/1eze8aE'>
-   </div>"
+  erb(:index)
 end
